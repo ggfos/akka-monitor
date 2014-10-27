cb198f85e94ea22a8adf0b03177235c30bdb0570
Mr.Tian <954155714@qq.com>
2014-10-27 16:13:44 +0800

    Update MailTester.scala

--- src/main/scala/com/ggfos/test/MailTester.scala
+++ src/main/scala/com/ggfos/test/MailTester.scala
@@ -10,7 +10,7 @@ object MailTester extends App {
 
   val mailInfo = MailSenderInfo(
     "smtp.qq.com", "25", //邮件服务器及端口号
-    "954155714@qq.com", "DoubleRound0517", true, //账号密码,及是否要经过验证
+    "954155714@qq.com", "***********", true, //账号密码,及是否要经过验证
     "954155714@qq.com", Iterable("214185383@qq.com", "954155714@qq.com"), //发送者及接收者
     "测试邮件!", "我是测试邮件!") //主题及内容
 
